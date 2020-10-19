6deeac7b2a897a9f277cd5eca9b5e7ffc8be9018
wacken <sebastianwalchi@gmail.com>
2020-10-19 07:31:50 +0200

    init

---
+++ Assets/Scripts/AI Behaviour/simpleMoveTo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Scripts.AI
+{
+    public class simpleMoveTo : MonoBehaviour
+    {
+        [SerializeField]
+        Transform _destination;
+
+        NavMeshAgent _navMeshAgent;
+
+        // Use this for initialization
+        void Start()
+        {
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            _navMeshAgent.SetDestination(_destination.position);
+        }
+    }
+
+}
